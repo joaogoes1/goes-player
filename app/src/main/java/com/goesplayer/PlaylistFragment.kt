3959bf642e58a96cb8994d82393ffb95ec3190a4
João Góes <joao_jdgf@hotmail.com>
2024-12-29 20:55:15 -0300

    mend

--- app/src/main/java/com/goesplayer/PlaylistFragment.kt
+++ app/src/main/java/com/goesplayer/PlaylistFragment.kt
@@ -39,85 +39,6 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-@Composable
-fun CreatePlaylistDialog(
-    name: String,
-    shouldShowDialog: MutableState<Boolean>,
-    dialogTextFieldValue: MutableState<String>,
-) {
-    AlertDialog(
-        title = {
-            Text(
-                stringResource(R.string.playlist_tab_create_playlist_dialog_title),
-                style = MaterialTheme.typography.titleSmall,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            )
-        },
-        text = {
-            TextField(
-                value = dialogTextFieldValue.value,
-                onValueChange = { dialogTextFieldValue.value = it },
-                modifier = Modifier.padding(horizontal = 16.dp),
-                colors = TextFieldDefaults.colors().copy(
-                    focusedContainerColor = Color.DarkGray,
-                    unfocusedContainerColor = Color.DarkGray,
-                )
-            )
-        },
-        onDismissRequest = { shouldShowDialog.value = false },
-        dismissButton = {
-            Button(onClick = {}) {
-                Text(stringResource(R.string.cancel))
-            }
-        },
-        confirmButton = {
-            Button(onClick = {}) {
-                Text(stringResource(R.string.confirme))
-            }
-        },
-    )
-}
-
-@Composable
-fun DeletePlaylistDialog(
-    name: String,
-    shouldShowDialog: MutableState<Boolean>,
-    confirmAction: (String) -> Unit,
-) {
-    AlertDialog(
-        onDismissRequest = { shouldShowDialog.value = false },
-        containerColor = Color(0xFF151515),
-        title = {
-            Text(
-                stringResource(R.string.delete_playlist_dialog_title),
-            )
-        },
-        text = {
-            Text(
-                stringResource(R.string.delete_playlist_dialog_text, name),
-            )
-        },
-        dismissButton = {
-            Button(onClick = { shouldShowDialog.value = false }) {
-                Text(
-                    stringResource(R.string.cancel),
-                    color = Color.Black,
-                )
-            }
-        },
-        confirmButton = {
-            Button(onClick = { confirmAction(name) }) {
-                Text(
-                    stringResource(R.string.confirme),
-                    color = Color.Black,
-                )
-            }
-        },
-    )
-}
-
 class PlaylistFragment : Fragment() {
     private val playlists = MutableLiveData(emptyList<String>())
     private val isLoading = MutableLiveData(false)
@@ -208,4 +129,82 @@ class PlaylistFragment : Fragment() {
         }
         Toast.makeText(requireContext(), toastText, Toast.LENGTH_LONG).show()
     }
+}
+@Composable
+fun CreatePlaylistDialog(
+    name: String,
+    shouldShowDialog: MutableState<Boolean>,
+    dialogTextFieldValue: MutableState<String>,
+) {
+    AlertDialog(
+        title = {
+            Text(
+                stringResource(R.string.playlist_tab_create_playlist_dialog_title),
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+        },
+        text = {
+            TextField(
+                value = dialogTextFieldValue.value,
+                onValueChange = { dialogTextFieldValue.value = it },
+                modifier = Modifier.padding(horizontal = 16.dp),
+                colors = TextFieldDefaults.colors().copy(
+                    focusedContainerColor = Color.DarkGray,
+                    unfocusedContainerColor = Color.DarkGray,
+                )
+            )
+        },
+        onDismissRequest = { shouldShowDialog.value = false },
+        dismissButton = {
+            Button(onClick = {}) {
+                Text(stringResource(R.string.cancel))
+            }
+        },
+        confirmButton = {
+            Button(onClick = {}) {
+                Text(stringResource(R.string.confirme))
+            }
+        },
+    )
+}
+
+@Composable
+fun DeletePlaylistDialog(
+    name: String,
+    shouldShowDialog: MutableState<Boolean>,
+    confirmAction: (String) -> Unit,
+) {
+    AlertDialog(
+        onDismissRequest = { shouldShowDialog.value = false },
+        containerColor = Color(0xFF151515),
+        title = {
+            Text(
+                stringResource(R.string.delete_playlist_dialog_title),
+            )
+        },
+        text = {
+            Text(
+                stringResource(R.string.delete_playlist_dialog_text, name),
+            )
+        },
+        dismissButton = {
+            Button(onClick = { shouldShowDialog.value = false }) {
+                Text(
+                    stringResource(R.string.cancel),
+                    color = Color.Black,
+                )
+            }
+        },
+        confirmButton = {
+            Button(onClick = { confirmAction(name) }) {
+                Text(
+                    stringResource(R.string.confirme),
+                    color = Color.Black,
+                )
+            }
+        },
+    )
 }
